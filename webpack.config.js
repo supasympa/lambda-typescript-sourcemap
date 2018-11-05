f77b3b3e7525e9c11c8b10156574df00f3c6a098
Lewis Barclay <lewis.barclay@googlemail.com>
2018-11-05 13:46:36 +0000

    Make sourcemaps work following packaging

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+// const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+// const debug = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: 'development',
+    context: __dirname,
+    entry: './handler.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.js',
+        library: "index",
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ["source-map-loader"],
+                enforce: "pre"
+            }
+        ]
+    },
+    target: 'node',
+    externals: [ nodeExternals() ],
+    devtool: "source-map",
+    plugins: [
+        // new webpack.optimize.UglifyJsPlugin({
+        //     mangle: !debug,
+        //     sourcemap: true
+        // }),
+    ],
+};
